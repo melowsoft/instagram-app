318a71f81261621e5c625cffc8f4650ec9db1a1b
Anthony Okpako <melowsworld@yahoo.co.uk>
2022-01-08 10:59:51 +0100

    Initial Commit

---
+++ Instagram-app/Instagram_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_appApp.swift
+//  Instagram-app
+//
+//  Created by Tony on 08/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
